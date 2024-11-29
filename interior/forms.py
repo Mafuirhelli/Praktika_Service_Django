04ea954e9dd7bb650a999376312b5f5efa36957f
Mafuirhelli <gr431_krrpa@lab-402-10.tomtit-tomsk.ru>
2024-11-29 10:05:39 +0700

    registration validation

--- interior/forms.py
+++ interior/forms.py
@@ -1,27 +1,41 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+
+
+class RussianValidator:
+    ALLOWED_CHARS = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщбыъэюя- "
+    code = 'russian'
+
+    def __init__(self, message=None):
+        self.message = message if message else "Должны присутствовать только русские символы, дефис и пробел."
+
+    def __call__(self, value):
+        if not (set(value) <= set(self.ALLOWED_CHARS)):
+            raise ValidationError(self.message, code=self.code, params={"value": value})
+
 
 class RegisterForm(UserCreationForm):
     username = forms.CharField(label='Логин')
+    first_name = forms.CharField(label="Имя", widget=forms.TextInput(attrs={'class': 'form-input'}), validators=[RussianValidator()], error_messages={'required': 'Введите имя'})
+    last_name = forms.CharField(label="Фамилия", widget=forms.TextInput(attrs={'class': 'form-input'}), validators=[RussianValidator()], error_messages={'required': 'Введите фамилию'})
     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput)\
+
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name','last_name', 'password1', 'password2']
         labels = {
             'email': 'E-mail',
-            'first_name': 'Имя',
-            'last_name': 'Фамилия',
         }
         widgets = {
             'email': forms.TextInput(attrs={'class': 'form-input'}),
-            'first_name': forms.TextInput(attrs={'class': 'form-input'}),
-            'last_name': forms.TextInput(attrs={'class': 'form-input'}),
         }
 
         def clean_password2(self):
             cd = self.cleaned_data
             if cd['password'] != cd['password2']:
-                raise forms.ValidationError("Пароли не совпадают!")
+                raise ValidationError("Пароли не совпадают!")
             return cd['password2']
+
