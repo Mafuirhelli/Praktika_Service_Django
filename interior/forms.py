fac0ac926c01dc054de2947a57747f0562d05af4
Mafuirhelli <gr431_krrpa@lab-402-10.tomtit-tomsk.ru>
2024-11-30 10:53:09 +0700

    2.1. complete

--- interior/forms.py
+++ interior/forms.py
@@ -37,14 +37,19 @@ class RegisterForm(UserCreationForm):
     username = forms.CharField(label='Логин')
     first_name = forms.CharField(label='Имя', widget=forms.TextInput(attrs={'class': 'form-input'}), validators=[RussianValidator()], error_messages={'required': 'Введите имя'})
     last_name = forms.CharField(label='Фамилия', widget=forms.TextInput(attrs={'class': 'form-input'}), validators=[RussianValidator()], error_messages={'required': 'Введите фамилию'})
+    patronym = forms.CharField(label='Отчество', widget=forms.TextInput(attrs={'class': 'form-input'}),
+                               validators=[RussianValidator()], error_messages={'required': 'Введите отчество'})
+    birthday = forms.DateField(label='День рождения', widget=forms.DateInput(attrs={'class': 'form-input'}),
+                               error_messages={'requied': 'Введите день рождения'})
     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name','last_name', 'password1', 'password2']
+        fields = ['username', 'email', 'first_name','last_name', 'patronym', 'birthday', 'password1', 'password2']
         labels = {
             'email': 'E-mail',
+            'patronym': 'Отчество'
         }
         widgets = {
             'email': forms.TextInput(attrs={'class': 'form-input'}),
--- interior/views.py
+++ interior/views.py
@@ -2,7 +2,7 @@ from bs4.diagnose import profile
 from django.contrib.auth import authenticate
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.shortcuts import render, redirect
-from .models import Query, Category
+from .models import Query, Category, Profile
 from django.views import generic
 from django.contrib.auth import authenticate, login
 from .forms import RegisterForm, ProfileForm, QueryForm, LoginForm, AddAdminForm
@@ -15,6 +15,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy,reverse
+
 
 def user_page(request):
     username = request.GET.get('username')
@@ -31,41 +32,49 @@ def index(request):
         'index.html',
         context={'num_queries_accepted':num_queries_accepted,},
     )
+
+def update_user_data(user):
+    Profile.objects.update_or_create(user=user, defaults={'patronym': user.profile.patronym, 'birthday': user.profile.birthday},)
+
+
 def sign_up(request):
     if request.method == 'GET':
         form = RegisterForm()
-        profile_form = ProfileForm(request.POST)
-        return render(request, 'interior/register.html', {'form': form, 'profile_form': profile_form})
+        return render(request, 'interior/register.html', {'form': form,})
     if request.method == 'POST':
         form = RegisterForm(request.POST)
-        profile_form = ProfileForm(request.POST)
-        if form.is_valid() and profile_form.is_valid():
-            user = form.save(commit=False)
-            profile = profile_form.save(commit=False)
-            user.username = user.username.lower()
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+
+            user.profile.patronym = form.cleaned_data['patronym']
+            user.profile.birthday = form.cleaned_data['birthday']
+            update_user_data(user)
+
             user.save()
-            profile.save()
+            user = authenticate(username=user.username, password=form.cleaned_data['password1'])
             messages.success(request, 'Вы успешно зарегестрировались')
             login(request, user)
             return redirect('index')
         else:
-            return render(request, 'interior/register.html', {'form': form, 'profile_form': profile_form})
+            return render(request, 'interior/register.html', {'form': form })
+
 
 def sign_in(request):
     if request.method == 'GET':
         form = LoginForm()
-        return render(request, 'interior/login.html', {'form': form})
-    if request.method == 'POST':
+        return render(request, 'interior/login.html', {'form': form,})
+    elif request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
             user = authenticate(request, username=username, password=password)
             if user:
                 login(request, user)
-                return redirect('userpage')
-        else:
-            return render(request, 'interior/register.html', {'form': form})
+                return redirect('index')
+            return render(request, 'interior/login.html', {'form': form,})
+
 
 def query_create(request):
     if request.method == 'GET':
@@ -80,8 +89,8 @@ def query_create(request):
             if request.user.is_authenticated:
                 username = request.user.username
             plan = form.cleaned_data.get("plan")
-            important = form.cleaned_data.get("important")
-            obj = Query.objects.create(title = title, description = description, category = category, plan = plan, author = User.objects.get(username = username))
+            creationDate = form.cleaned_data.get("creationDate")
+            obj = Query.objects.create(title = title, description = description, category = category, plan = plan, creationDate = creationDate, author = User.objects.get(username = username))
             obj.save()
             messages.success(request, 'Вы успешно создали заявку')
             return redirect('index')
@@ -89,11 +98,13 @@ def query_create(request):
             return render(request, 'interior/query_form.html', {'form': form})
 class QueryDetailView(generic.DetailView):
     model = Query
+
 class QueryListView(LoginRequiredMixin,generic.ListView):
     model = Query
     template_name ='interior/query_list.html'
     def get_queryset(self):
-        return Query.objects.filter(author=self.request.user).ordered_by('-important')
+        return Query.objects.filter(author=self.request.user).ordered_by('-creationDate')
+
 class QueryDelete(DeleteView):
     success_url = reverse_lazy('querys')
 
@@ -101,17 +112,19 @@ class QueryListViewN(LoginRequiredMixin,generic.ListView):
     model = Query
     template_name ='interior/query_list_n.html'
     def get_queryset(self):
-        return Query.objects.filter(author=self.request.user).order_by('-important').filter(status='n')
+        return Query.objects.filter(author=self.request.user).filter(status='n').order_by('-creationDate')
+
 class QueryListViewA(LoginRequiredMixin,generic.ListView):
     model = Query
     template_name ='interior/query_list_a.html'
     def get_queryset(self):
-        return Query.objects.filter(author=self.request.user).order_by('-important').filter(status='a')
+        return Query.objects.filter(author=self.request.user).filter(status='a').order_by('-creationDate')
+
 class QueryListViewD(LoginRequiredMixin,generic.ListView):
     model = Query
     template_name ='interior/query_list_d.html'
     def get_queryset(self):
-        return Query.objects.filter(author=self.request.user).order_by('-important').filter(status='d')
+        return Query.objects.filter(author=self.request.user).filter(status='d').order_by('-creationDate')
 
 class QueryAddDesign(UpdateView):
     model = Query
@@ -134,9 +147,9 @@ class CategoryCreateView(CreateView):
 
 class AdminQueryListView(LoginRequiredMixin,generic.ListView):
     model = Query
-    template_name ='interior/admin_plea_list.html'
+    template_name ='interior/admin_query_list.html'
     def get_queryset(self):
-        return Query.objects.order_by('-important')
+        return Query.objects.order_by('-creationDate')
 
 
 class QueryAddDesign(UpdateView):
@@ -147,9 +160,9 @@ class QueryAddDesign(UpdateView):
 
 class SuperuserQueryListView(LoginRequiredMixin,generic.ListView):
     model = Query
-    template_name ='interior/superuser_plea_list.html'
+    template_name ='interior/superuser_query_list.html'
     def get_queryset(self):
-        return Query.objects.filter(status='n').order_by('-important')
+        return Query.objects.filter(status='n').order_by('-creationDate')
 
 def QueryAddAdmin(request, pk):
     query = get_object_or_404(Query, pk=pk)
