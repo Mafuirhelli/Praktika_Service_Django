da45ee609e5ad4506ee3bd9f4f54b60f58ed98a3
Mafuirhelli <kot35843@gmail.com>
2024-12-03 09:55:12 +0700

    rusification

--- interior/models.py
+++ interior/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from datetime import date
-from django.db.models import ForeignKey
+from django.db.models import ForeignKey, ManyToManyField
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
@@ -9,6 +9,7 @@ from django.dispatch import receiver
 from django.urls import reverse
 from django.db.models import UniqueConstraint
 from django.db.models.functions import Lower
+
 class Profile(models.Model):
     user = models.OneToOneField(
         User,
@@ -37,7 +38,7 @@ def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
 
 class Category(models.Model):
-    name = models.CharField(max_length=200, unique=True, help_text="Enter a category of the request (3D Design, 2D Design, etc.)")
+    name = models.CharField(max_length=200, unique=True, help_text="Введите категорию заявок (2Д дизайн, 3Д дизайн и т.д.)")
     def __str__(self):
         return self.name
     def get_absolute_url(self):
@@ -45,11 +46,12 @@ class Category(models.Model):
 
 class Query(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField(max_length=1000, help_text="Type a brief description of the interior")
-    category = ForeignKey('Category', on_delete=models.SET_NULL, null=True, help_text="Выберите категорию заявки")
-    plan = models.ImageField(upload_to ='images/', help_text="Select a cover for this request", null=True)
+    description = models.TextField(max_length=1000, help_text="Пропишите краткое описание интерьера")
+    category = ManyToManyField('Category', help_text="Выберите категорию заявки")
+    plan = models.ImageField(upload_to ='images/', help_text="Выберите обложку для заявки", null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     creationDate = models.DateField(default=date.today)
+    # test = models.TextField(default='qwerty')
     design = models.ImageField(upload_to ='images/', null=True)
     LOAN_STATUS = (
         ('n', 'New'),
